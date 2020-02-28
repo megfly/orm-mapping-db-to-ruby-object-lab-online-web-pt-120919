ad98a7adc5d4b85d74aeb04dcbdc5247ad7709a7
Meghan Flynn <mflynn333@gmail.com>
2020-02-28 23:50:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -92,8 +92,7 @@ class Student
     def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT * FROM students 
-      WHERE grade = 10 
-      LIMIT ?
+      WHERE grade = ?
     SQL
     
     DB[:conn].execute(sql, grade).map do |row|
