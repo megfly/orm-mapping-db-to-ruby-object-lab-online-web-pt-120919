f19da584318de42f9f05a7d9ed49dce541489e92
Meghan Flynn <mflynn333@gmail.com>
2020-02-28 23:40:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,6 +55,17 @@ class Student
     end
   end 
   
+    def self.all_students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students 
+      WHERE grade < 12
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end 
+  
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
