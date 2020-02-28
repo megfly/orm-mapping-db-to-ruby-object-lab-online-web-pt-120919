61a5a2e8431b77a50b26a44d6bc093f9c6b5c4a2
Meghan Flynn <mflynn333@gmail.com>
2020-02-28 23:49:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,6 +89,18 @@ class Student
     end.first
   end 
   
+    def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT * FROM students 
+      WHERE grade = 10 
+      LIMIT ?
+    SQL
+    
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end 
+  
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
